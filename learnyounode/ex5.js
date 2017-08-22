cd8688a05c8b89bc61ced1515aa046f9ca697dd1
Ada Melentyeva <ada.melentyeva@gmail.com>
2017-08-22 16:48:26 +0300

    Ex. 5: done

--- learnyounode/ex5.js
+++ learnyounode/ex5.js
@@ -1,7 +1,12 @@
 'use strict';
 
 const fs = require('fs');
+const path = require('path');
 
 fs.readdir(process.argv[2], (err, lines) => {
-  console.log(lines);
+  for (let i = 0; i < lines.length; i++) {
+    if (path.extname(lines[i]) === '.' + process.argv[3]) {
+      console.log(lines[i]);
+    }
+  }
 });
