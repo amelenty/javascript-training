4b701f3f71a6919c58ef9087bf0a80c2f8885d28
Ada Melentyeva <ada.melentyeva@gmail.com>
2017-08-22 16:36:47 +0300

    Ex. 4: done

--- learnyounode/ex4.js
+++ learnyounode/ex4.js
@@ -1,3 +1,8 @@
 'use strict';
 
 const fs = require('fs');
+
+fs.readFile(process.argv[2], 'utf8', (err, fileContents) => {
+  const linesNumber = fileContents.split('\n').length - 1;
+  console.log(linesNumber);
+});
