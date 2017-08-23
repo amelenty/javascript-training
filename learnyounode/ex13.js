a776f7175b6ae712103f568a2fa0ec6121d7d61c
Ada Melentyeva <ada.melentyeva@gmail.com>
2017-08-23 15:27:02 +0300

    All challenges done!

--- learnyounode/ex13.js
+++ learnyounode/ex13.js
@@ -2,26 +2,34 @@
 
 const http = require('http');
 const url = require('url');
+const Readable = require('stream').Readable;
 
 const port = process.argv[2];
 
 
 const server = http.createServer((request, response) => {
   const urlData = url.parse(request.url, true);
-  let time = {}
+  const time = {};
+  const date = new Date(urlData.query.iso);
+  const outStream = new Readable();
+  outStream._read = function noop() {};
   switch (urlData.pathname) {
     case '/api/parsetime':
-      // parsing logic
+      time.hour = date.getHours();
+      time.minute = date.getMinutes();
+      time.second = date.getSeconds();
       break;
     case '/api/unixtime':
-      // unixtime logic
+      time.unixtime = date.getTime();
       break;
     default:
       console.err('Sorry, wrong URL!');
       break;
   }
   response.writeHead(200, { 'Content-Type': 'application/json' });
-  time.stringify().pipe(response);
+  outStream.push(JSON.stringify(time));
+  outStream.push(null);
+  outStream.pipe(response);
 });
 
 server.listen(port);
