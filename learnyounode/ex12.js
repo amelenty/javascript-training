7b8d0f0dea3b45d99e452b7a48ff473f3dd2b816
Ada Melentyeva <ada.melentyeva@gmail.com>
2017-08-23 14:42:58 +0300

    Ex. 12: done

---
+++ learnyounode/ex12.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const http = require('http');
+const map = require('through2-map');
+
+const port = process.argv[2];
+
+
+const server = http.createServer((request, response) => {
+  request.pipe(map((chunk) => chunk.toString().toUpperCase())).pipe(response);
+});
+
+server.listen(port);
